70184d08a8c2479c7a3fb5c6d408e4cf0e19570c
WolfPackStatMathieu <mthomassin747@gmail.com>
2022-05-13 14:27:23 +0200

    commit

--- lecture_csv.py
+++ lecture_csv.py
@@ -1,14 +1,16 @@
 from fileinput import filename
 import gzip
 import csv
-
+import numpy as np
 #Dossier où se trouve le fichier :
 folder = ''
 filename = 'synop.201301.csv.gz'
 data = []
 with gzip.open(folder + filename, mode='rt') as gzfile :
-    synopreader = csv.reader(gzfile, delimiter = ';')
+    #.readlines()[1:3] pour ne lire que les 3 premières lignes
+    synopreader = csv.reader(gzfile.readlines()[0:3], delimiter = ';')
     for row in synopreader :
         data.append(row)
-
-print(data[-1])
+print(data)
+data_np = np.array(data)
+print(data_np)
