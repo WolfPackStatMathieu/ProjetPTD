5fc43613369a87f83864ea0bfbfdcf0f38fd004d
Frank2051 <27kan.francis@gmail.com>
2022-05-14 20:16:13 +0200

    pipeline et operation

---
+++ src/pipeline.py
@@ -0,0 +1,30 @@
+from numpy import array
+from donnees import np, Donnees
+from operation import Operation
+''''''
+class Pipeline:
+    def __init__(self,etapes, resultat = Donnees([],array([]))) :
+        self.etapes = etapes
+        self.resultat = resultat
+    
+    def get_pip(self):
+        return self.etapes
+    
+    def get_res(self):
+        return self.resultat
+    
+    def add_ope(self,operation):
+        self.etapes.append(operation)
+    
+    def del_ope(self):
+        self.etapes.pop()
+    
+    def fus_pip(self, autre_pipeline):
+        self.etapes += autre_pipeline.etapes
+    
+    def execute(self):
+        for commande in self.etapes :
+            commande.ope()
+        
+
+    
