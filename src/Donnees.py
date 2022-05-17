39cb8a0e7e56d96ac36c339eb2d1208ecab0ecf5
Frank2051 <27kan.francis@gmail.com>
2022-05-17 14:43:43 +0200

    comment

--- src/Donnees.py
+++ src/Donnees.py
@@ -1,7 +1,35 @@
-''''''
+'''module Donnees pour représenter un jeu de données
+'''
 import numpy as np
 
 class Donnees :
+
+    '''représente un jeu de données
+
+    Parameters
+    ----------
+    variables : list[str]
+        Liste des noms de variables
+    data : np.array
+        donnees du jeu de donnees
+
+    Attributes
+    ----------
+    variables : list[str]
+        Liste des noms de variables
+    data : np.array
+        donnees du jeu de donnees
+    var_types : list[type]
+        liste des types des variables
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> donnees = Donnees(['nom', 'valeur'],
+    ...                    np.array([['a',1], ['b', 5, ], ['c',9]]))
+
+
+    '''
         def __init__(self, variables , data):
             self.variables = variables
             self.data = data
@@ -74,38 +102,6 @@ class Donnees :
                 if not np.nan in x :
                     ajout[i] = transformation(tuple(x))
             self.add_var([nom],ajout)
-'''module Donnees pour représenter un jeu de données
-'''
-import numpy as np
-
-class Donnees :
-    '''représente un jeu de données
-
-    Parameters
-    ----------
-    variables : list[str]
-        Liste des noms de variables
-    data : np.array
-        donnees du jeu de donnees
-
-    Attributes
-    ----------
-    variables : list[str]
-        Liste des noms de variables
-    data : np.array
-        donnees du jeu de donnees
-
-    Examples
-    --------
-    >>> import numpy as np
-    >>> donnees = Donnees(['nom', 'valeur'],
-    ...                    np.array([['a',1], ['b', 5, ], ['c',9]]))
-
-
-    '''
-    def __init__(self, variables, data):
-        self.variables = variables
-        self.data = data
 
 
 if __name__ == '__main__':
