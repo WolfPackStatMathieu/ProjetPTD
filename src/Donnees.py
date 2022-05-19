9add94fac0a250f189a78b3fe4727b235fdc864a
Frank2051 <27kan.francis@gmail.com>
2022-05-19 17:02:11 +0200

    plus que filtre et transform

--- src/Donnees.py
+++ src/Donnees.py
@@ -205,37 +205,59 @@ class Donnees :
         
         Parameters
         ----------
-        autres_donnees : Donnees
-            donnees à concaténer
-
+        parametres : list[str]
+            liste de parametres pris parmis les variables du jeu de donnees
+        test_filtre : function
+            fonction qui renvoit un booleen, utiliser de préférence une lambda function
+        keep_na : bool
+            décide si on garde les lignes où un des paramètres manque
+        
         Examples
         --------
         >>> import numpy as np
-        >>> test = Donnees('nom',['nom', 'valeur'],[['a',1], ['b', 5 ], ['c',9]])
-        >>> test2 =  Donnees('nom2',['nom', 'valeur'],[['d',10], ['e', 50 ], ['f',90], ['z',100]])
-        >>> test.concat(test2)
+        >>> test = Donnees('nom',['nom', 'valeur'],[['a',1], ['b', 5 ], ['c',9],['d',10], ['e', 50 ], ['f',90], ['z',100]])
+        >>> test.filtre(['valeur'],lambda x : 0 >= 50)
         >>> print(test)
-        [['a' 1]
-         ['b' 5]
-         ['c' 9]
-         ['d' 10]
-         ['e' 50]
+        [['e' 50]
          ['f' 90]
          ['z' 100]]
         '''      
-        
-        # on utilise un compteur pour éviter les problèmes d'index après suppresion lors du parcours
+        # on utilise un compteur pour éviter les problèmes d'index après suppresion de ligne
+        # lors du parcours
         i=0
         while i <self.data.shape[0]:
             x = []
             for p in parametres :
                 x.append(self.data[i,self.get_var(p)])
-            if (np.nan in x and not keep_na) or not test_filtre(tuple(x)):
+            if (np.nan in x and not keep_na) or not test_filtre(x):
                 np.delete(self.data,i ,0)
             else :
                 i+=1
 
-    def transform(self,nom, parametres, transformation, keep_na = False):
+    def transform(self,nom, parametres, transformation):
+        '''crée une nouvelle variable et ajoute une colonne au jeu de données composée des valeur qu'on a calculé
+        
+        Parameters
+        ----------
+        parametres : list[str]
+            liste de parametres pris parmis les variables du jeu de donnees
+        transformation : function
+            fonction qui renvoit la valeur de la nouvelle variable pour cette ligne
+        nom : str
+            nom de la nouvelle variable
+        
+        Examples
+        --------
+        >>> import numpy as np
+        >>> test = Donnees('nom',['nom', 'valeur'],[['a',1], ['b', 5 ], ['c',9],
+        ['d',10], ['e', 50 ], ['f',90], ['z',100]])
+        >>> test.transform('nouveau',['valeur'], lambda x : x + 2)
+        >>> print(test)
+        [['e' 50 52]
+         ['f' 90 92]
+         ['z' 100 102]]
+        '''      
+
         assert not nom in self.variables
         ajout = np.full((self.data.shape[0],1),np.nan)
         for i in range(self.data.shape[0]):
