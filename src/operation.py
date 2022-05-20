7a2f97940e24d638acd7e89eab7c77c488f0a22b
Frank2051 <27kan.francis@gmail.com>
2022-05-20 16:57:41 +0200

    operation ok

--- src/operation.py
+++ src/operation.py
@@ -1,17 +1,13 @@
+'''module contenant la classe abstraite des opérations'''
 from pipeline import Pipeline
 from abc import ABC, abstractmethod
 
 class Operation(ABC):
     """classe abstraite Opération
-
-    Parameters
-    ----------
-    ABC : _type_
-        _description_
     """
     @abstractmethod
     def ope(self, pipeline):
-        """classe ope vide
+        """méthode ope vide
 
         Parameters
         ----------
