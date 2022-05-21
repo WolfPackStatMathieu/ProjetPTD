9f9a6eb5d59b9f305e5a21b9b78cbfd23387b3a1
Frank2051 <27kan.francis@gmail.com>
2022-05-21 16:22:32 +0200

    diagramme

--- src/transformation/jointure.py
+++ src/transformation/jointure.py
@@ -1,9 +1,12 @@
-'''module pour joindre un jeu de donnese abev un autre'''
+'''module pour joindre un jeu de donnes avec un autre'''
 from transformation import Transformation
 
 class Jointure(Transformation):
     '''classe d'opération permettant de joindre des jeux de donnees'''
-    def __init__(self,autre_donnees ): 
+    def __init__(self,autre_donnees, keys ): 
         self.autre_donnes = autre_donnees
+        self.keys = keys
     
+    def ope(pipeline):
+        
     
