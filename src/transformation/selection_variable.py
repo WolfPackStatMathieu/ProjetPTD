1d1061796e874ed43f0f75a0e52af8c4a0c43a1d
Frank2051 <27kan.francis@gmail.com>
2022-05-28 17:29:49 +0200

    transformations finis, plus qu'a documenter

--- src/transformation/selection_variable.py
+++ src/transformation/selection_variable.py
@@ -1,10 +1,13 @@
 '''module de selection des variables'''
 from transformation import Transformation
+from src.pipeline import Pipeline
 
 class Selection__var(Transformation):
 
     def __init__(self, selection):
         self.selection = selection
     
-    def ope(pipeline):
-        pass
+    def ope(self,pipeline : Pipeline):
+        for v in pipeline.resultat.variables:
+            if not v in self.selection:
+                pipeline.resultat.del_var([v])
