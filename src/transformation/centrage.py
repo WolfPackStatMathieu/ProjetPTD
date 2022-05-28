420d1ed6a29044e48832206b2e7ecb90265bccc8
Frank2051 <27kan.francis@gmail.com>
2022-05-28 17:10:13 +0200

    centrage et variance

--- src/transformation/centrage.py
+++ src/transformation/centrage.py
@@ -1,9 +1,8 @@
 from src.pipeline import Pipeline
 from transformation import Transformation
 from estimation.moyenne import Moyenne
-from estimation.variance import Variance
 
-class Normalisation(Transformation):
+class Centrage(Transformation):
 
     def __init__(self, variables):
         self.variables = variables
@@ -11,5 +10,4 @@ class Normalisation(Transformation):
     def ope(self,pipeline : Pipeline):
         for v in self.variables:
             v_moyen = Moyenne([v]).ope(Pipeline('',[v],pipeline.resultat.data[:][pipeline.resultat.get_var(v)]))
-            v_variance = Variance([v]).ope(Pipeline('',[v],pipeline.resultat.data[:][pipeline.resultat.get_var(v)]))
-            pipeline.resultat.transform(v +'_centr',[v],lambda x : (x[0] - v_moyen)/ v_variance)
+            pipeline.resultat.transform(v +'_centr',[v],lambda x : x[0] - v_moyen)
