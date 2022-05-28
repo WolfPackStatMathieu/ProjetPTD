95492a8c699ea3c9139ee7c2550265196f90d27e
Frank2051 <27kan.francis@gmail.com>
2022-05-28 15:37:09 +0200

    presque ok

--- src/transformation/aggregation_spatiale.py
+++ src/transformation/aggregation_spatiale.py
@@ -1,9 +1,12 @@
+from src.pipeline import Pipeline
 from transformation import Transformation
 
 class Aggreg(Transformation):
-    def __init__(self, space_var, echelle = "région") :
+    def __init__(self, space_var, echelle = "region") :
         self.echelle = echelle
         self.space_var = space_var
     
-    def ope(pipeline):
-        pass
+    def ope(self,pipeline : Pipeline):
+        def aggregat():
+            pass
+        pipeline.resultat.transform(self.space_var + "_aggr",[self.space_var],aggregat())
--- src/transformation/moyenne_glissante.py
+++ src/transformation/moyenne_glissante.py
@@ -1,1 +1,21 @@
+from src.Donnees import Donnees
+from src.pipeline import Pipeline
 from transformation import Transformation
+import numpy as np
+from estimation.moyenne import Moyenne
+
+class Moyenne_Glissante(Transformation):
+
+    def __init__(self, variables, pas):
+        self.variables=variables
+        self.pas = pas
+
+    def ope(self, pipeline : Pipeline):
+        i = 0
+        def glissante(var):
+            if i < self.pas or i > pipeline.resultat.data.shape[0] - self.pas - 1 :
+                
+                return np.nan
+            else:
+                return Moyenne([var]).ope(pipeline)
+        
