27e4d3eac05aa1495ac23a6994c93d9dac475027
Frank2051 <27kan.francis@gmail.com>
2022-05-29 20:37:34 +0200

    fenetrage

--- src/package_transformation/fenetrage.py
+++ src/package_transformation/fenetrage.py
@@ -1,13 +1,43 @@
 from transformation import Transformation
 from src.pipeline import Pipeline
+import datetime as dt
 
 class Fenetrage(Transformation):
+    '''classe de l'opération de fenetrage qui enleve les observations en dehors de la fenetre temporelle
 
-    def __init__(self,debut, fin, time_var,echantillon = "keep", ):
+    Parameters
+    ----------
+    debut : datetime
+        debut de la fenetre temporelle
+    fin : datetime
+        fin de la fenetre temporelle
+    time_var : variable temporelle
+
+    Attributes
+    ----------
+
+    debut : datetime
+        debut de la fenetre temporelle
+    fin : datetime
+        fin de la fenetre temporelle
+    time_var : variable temporelle
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> mes_donnees_1 = Donnees('mon_nom_jeu_de_donnees',['nom', 'time'],[['a',dt.datetime(2001,1,1,0,0)], ['b',dt.datetime(2001,1,15,0,0) ], ['c',dt.datetime(2001,5,5,0,0)]])
+    >>> mon_pipeline = Pipeline([Fenetrage(dt.datetime(2000,1,1,0,0),dt.datetime(2001,4,1,0,0),'time')], mes_donnes_1)
+    >>> mon_pipeline.execute()
+    >>> print(mon_pipeline)
+    [['a' datetime(2001,1,1,0,0)]
+     ['b' datetime(2001,1,15,0,0)]]  
+
+
+    '''
+    def __init__(self,debut, fin, time_var ):
         self.debut = debut
         self.fin = fin
         self.time_var = time_var
-        self.echantillon = echantillon
 
     def ope(self, pipeline : Pipeline):
         '''méthode qui permet d'exécuter l'opération
